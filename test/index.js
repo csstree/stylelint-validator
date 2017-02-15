dadeabbce84e22c854f28e43af57b9dcdf9b0249
Roman Dvornov <rdvornov@gmail.com>
2017-02-16 02:23:39 +0300

    add some tests

--- test/index.js
+++ test/index.js
@@ -12,10 +12,12 @@ var sass = ruleTester(validator.rule, validator.ruleName, {
 // base test
 css(null, function(tr) {
     tr.ok('.foo { color: red }');
+    tr.ok('.foo { color: #123456 }');
     tr.notOk('.foo { color: red green }', messages.uncomplete('color'));
     tr.notOk('.foo { color: 1 }', messages.invalid('color'));
     tr.notOk('.foo { color: #12345 }', messages.invalid('color'));
     tr.notOk('.foo { color: &a }', messages.parseError('&a'));
+    tr.notOk('.foo { baz: 123 }', 'Unknown property: baz');
 });
 
 // ignore values with less extenstions
